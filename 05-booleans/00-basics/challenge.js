605d3c5e2497b1da93b62f474f6665892573f5bf
vagrant <vagrant@rails-dev-box>
2014-10-14 16:16:40 +0000

    boolean exercise

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,19 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+	return stringOne == stringTwo;
 };
 
 module.exports.notEqual = function(one, two) {
-
+	return one !== two;
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+	return upper >= middle >= lower;
 };
 
 module.exports.outsideRanges = function(number) {
-
+	return !((21 > number && number > 9) || (76 > number & number > 42) || (6 > number && number > 2))
 };
 
 module.exports.nameAndPrice = function(name, price) {
-
+	return (name == "NYTimes" || name == "LATimes") && price >= 1;
 };
