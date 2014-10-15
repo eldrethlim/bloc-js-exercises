960e56f8c3c6f6e30335390dc6dfa2425b418dad
Jun Xian <eldrethlim@gmail.com>
2014-10-15 04:22:25 +0000

    array exercises

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,21 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(item,list){
+  itemIsHere = false;
+  
+  for (i = 0; i < list.length; i++) {
+    if (list[i] == item)
+      itemIsHere = true;
+  };
 
-module.exports.reverseSortedList = undefined;
+  if (itemIsHere == true) {
+    return list
+  }
+  else {
+    list.push(item);
+    return list;
+  }
+};
+
+module.exports.reverseSortedList = function(array){
+  array.sort().reverse();
+  return array;
+};
