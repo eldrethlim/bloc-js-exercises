b05a55f168a6be00ed51e102fef641cdb422af26
Jun Xian <eldrethlim@gmail.com>
2014-10-15 14:06:57 +0000

    object exercises

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,37 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents){
+  
+  var course = {
+    title:  courseTitle,
+    duration: courseDuration,
+    students: courseStudents
+  };
 
-module.exports.addProperty = undefined;
+  return course;
+};
 
-module.exports.formLetter = undefined;
+module.exports.addProperty = function(object, newProp, newValue){
 
-module.exports.canIGet = undefined;
+  if (object[newProp] == null) {
+    object[newProp] = newValue;
+    return object;
+  }
+  else {
+    return object;
+  }
+};
+
+module.exports.formLetter = function(letter){
+
+  return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+};
+
+module.exports.canIGet = function(item, money){
+  var priceChart = {
+    "MacBook Air": 999,
+    "MacBook Pro": 1299,
+    "Mac Pro": 2499,
+    "Apple Sticker": 1
+  };
+
+  return money >= priceChart[item];
+};
